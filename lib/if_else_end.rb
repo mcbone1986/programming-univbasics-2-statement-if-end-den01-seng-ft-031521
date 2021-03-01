a2aa17c1a38fdd7f4975051b544fefc2e8f0c500
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-01 20:25:36 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,8 @@
-if false
-  puts "This code does not run. You will not see this in IRB"
+chance_of_rain = 0.2 
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
 end
